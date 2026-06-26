55eea2f41bff178ec0126677eeca997d90f9c492
Sammo Talukdar <sammotalukdar@gmail.com>
2026-06-27 03:18:24 +0600

    Implement recursive sum function in sum.c
    Added a recursive function to calculate the sum of integers up to n.

--- Lab - 01 /sum.c
+++ Lab - 01 /sum.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int  sum(int n)
+{
+    if(n==1)
+        return 1;
+    return n+sum(n-1);
+}
+int main()
+{
+    int  n;
+    printf("Enter the number of element you want to sum up :");
+      scanf("%d",&n);
+     
+     printf("Sum=%d",sum(n));
+   
+    return 0;
+}
